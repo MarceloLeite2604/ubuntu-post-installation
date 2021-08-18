f1d04382f01ee13fd7cdab149ddde3778580d8a5
Marcelo Leite <MarceloLeite2604@users.noreply.github.com>
2021-08-18 17:37:39 -0300

    Create function to load and unload constants

--- commons/constants.sh
+++ commons/constants.sh
@@ -7,3 +7,30 @@
 SUCCESS=0
 FAILURE=255
 SKIP=254
+
+load_constants() {
+  local file_path=$1;
+
+  if [[ -f $file_path ]]; then
+    log_error "File \"$file_path\" not found."
+    return $FAILURE;
+  fi;
+
+  while read property; do
+    eval export readonly $property;
+  done <$file_path;
+}
+
+unload_constants() {
+  local file_path=$1;
+
+  if [[ -f $file_path ]]; then
+    log_error "File \"$file_path\" not found."
+    return $FAILURE;
+  fi;
+
+  while read property; do
+    var_name=${property%=*};
+    eval unset $var_name;
+  done <$file_path;
+}
