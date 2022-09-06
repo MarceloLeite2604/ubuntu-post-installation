1a7e0e0d296d667250503d268620f19947c3df7b
MarceloLeite2604 <marceloleite2604@gmail.com>
2022-09-06 00:33:10 -0300

    Add interface configuration

---
+++ scripts/20-disable-dock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide 'false';
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed 'false';
+
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide 'false';
---
+++ scripts/21-hide-top-bar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install gnome-shell-extensions gnome-shell-extension-manager -y;
+
+echo "Now open Gnome Shell Extension Manager and search for \"Hide Top Bar\".";
+echo "This extension is maintained by tuxor1337.";
+echo "Sorry, buddy! This one has to be done manually. ¯\_(ツ)_/¯";
